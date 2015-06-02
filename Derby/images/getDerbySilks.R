d56ac6e468cf5a6a4438d9e4b425f244c568283c
Tom Heslop <t_heslop@hotmail.co.uk>
2015-06-02 20:15:08 +0100

    update R script

--- Derby/images/getDerbySilks.R
+++ Derby/images/getDerbySilks.R
@@ -1,9 +1,35 @@
-# Runner Silks
+# Derby Runner Silks
 
 library(rvest)
 
 # Timeform URL
 tf_url <- "https://www.timeform.com/racing/RaceCard/Race/2015-06-06/16/5"
+
+# read page
+doc <- html(tf_url)
+
+# silk urls
+silks <- doc %>% 
+    html_nodes(css = ".silksImg") %>%
+    html_attr("src")
+
+# runners
+runners <- doc %>%
+    html_nodes(css = "#raceCardEntries strong") %>%
+    html_text() %>%
+    stringr::str_replace_all(" \\([[:alpha:]]{2,3}\\)", "") %>%
+    stringr::str_replace_all("\\s+", "-") %>%
+    tolower() %>%
+    paste0(".png")
+
+for(silk in 1:length(silks)) {
+    download.file(url = silks[silk], destfile = runners[silk], mode = "wb")
+}
+
+###############
+# Oaks Runner Silks
+
+tf_url <- "https://www.timeform.com/racing/RaceCard/Race/2015-06-05/16/5"
 
 # read page
 doc <- html(tf_url)
