725ad0a686e1332fbfb03fa0054d126d4a07ca52
Tom Heslop <t_heslop@hotmail.co.uk>
2015-06-07 20:10:32 +0100

    add Derby sectionals

--- Derby/data/prep_data.R
+++ Derby/data/prep_data.R
@@ -2,6 +2,7 @@ library(RcappeR)
 library(dplyr)
 library(jsonlite)
 
+# Oaks Sectionals, manufactored using beaten lengths ---------------------------
 Oaks
 tmp <- Oaks %>%
     mutate(c5f = conv_margins(btn_l = len5f, win_time = 72.36, conditions = "f-gq"),
@@ -32,3 +33,35 @@ tmp <- Oaks %>%
 y <- plyr::dlply(tmp, "pos", as.list)
 y <- unname(y)
 write(x = toJSON(y, auto_unbox = T), file = "Oaks2015.json")
+
+# Derby Sectionals, manufactored using beaten lengths & video -----------------
+Derby
+tmp <- Derby %>%
+    mutate(c5f = c5f,
+           c8_5f = conv_margins(btn_l = len8_5f, win_time = 112.88, conditions = "f-gq"),
+           c10f = conv_margins(btn_l = len10f, win_time = 129.44, conditions = "f-gq"),
+           c11f = c11f, 
+           c12f = conv_margins(btn_l = len12f, win_time = 152.32, conditions = "f-gq"),
+           s5f = c5f,
+           s8_5f = c8_5f - c5f,
+           s10f = c10f - c8_5f,
+           s11f = c11f - c10f,
+           s12f = c12f - c11f,
+           fs7f = fin_spd(fin_time = c12f, dist = 12, sect_time = c12f - c5f, sect_dist = 7),
+           fs3_5f = fin_spd(fin_time = c12f, dist = 12, sect_time = c12f - c8_5f, sect_dist = 3.44),
+           fs2f = fin_spd(fin_time = c12f, dist = 12, sect_time = c12f - c10f, sect_dist = 2),
+           fs1f = fin_spd(fin_time = c12f, dist = 12, sect_time = c12f - c11f, sect_dist = 1),
+           bl5f = btn_sec(c5f),
+           bl8_5f = btn_sec(c8_5f),
+           bl10f = btn_sec(c10f),
+           bl11f = btn_sec(c11f),
+           bl12f = btn_sec(c12f),
+           p5f = rank(c5f, ties.method = "first"),
+           p8_5f = rank(c8_5f, ties.method = "first"),
+           p10f = rank(c10f, ties.method = "first"),
+           p11f = rank(c11f, ties.method = "first"),
+           p12f = rank(c12f, ties.method = "first"))
+
+y <- plyr::dlply(tmp, "pos", as.list)
+y <- unname(y)
+write(x = toJSON(y, auto_unbox = T), file = "Derby2015.json")
