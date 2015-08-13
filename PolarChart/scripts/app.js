79b5febe3b3c6a3f33e01730f086c17966685e92
Tom Heslop <t_heslop@hotmail.co.uk>
2015-08-13 22:55:16 +0100

    initial Polar Chart

---
+++ PolarChart/scripts/app.js
@@ -0,0 +1,10 @@
+// var config = {
+//     height: 500,
+//     width: 500
+// }
+//
+// var vis = d3.select('#radial')
+//     .append('svg')
+//     .attr('height', config.height)
+//     .attr('width', config.width)
+//     .style('background-color', '#000000');
