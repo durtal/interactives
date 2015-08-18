873ebc1be7e482f5d43a54ba5f5d20a03647c98f
Tom Heslop <t_heslop@hotmail.co.uk>
2015-08-18 20:04:10 +0100

    chart

--- PolarChart/scripts/app.js
+++ PolarChart/scripts/app.js
@@ -1,10 +1,14 @@
-// var config = {
-//     height: 500,
-//     width: 500
-// }
-//
-// var vis = d3.select('#radial')
-//     .append('svg')
-//     .attr('height', config.height)
-//     .attr('width', config.width)
-//     .style('background-color', '#000000');
+d3.json('data/data.json', function(err, data) {
+    console.log(data);
+    var chart = radialBarChart()
+        .barHeight(250)
+        .reverseLayerOrder(true)
+        .capitaliseLabels(true)
+        .barColours(['#B66199', '#9392CB', '#76D9FA', '#BCE3AD', '#FFD28C', '#F2918B'])
+        .domain([0,10])
+        .tickValues([1,2,3,4,5,6,7,8,9,10])
+        .tickCircleValues([1,2,3,4,5,6,7,8,9]);
+    d3.select('#chart')
+        .datum(data)
+        .call(chart);
+});
